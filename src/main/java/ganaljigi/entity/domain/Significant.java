a011bca04eb56a0807d5de66551a8ef2998e7d28
choihongjun <vvclsbalam@gmail.com>
2025-05-25 04:19:58 +0900

    feat: 건물 특이사항 사진 엔티티 추가

--- src/main/java/ganaljigi/entity/domain/Significant.java
+++ src/main/java/ganaljigi/entity/domain/Significant.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -18,4 +21,7 @@ public class Significant {
     @ManyToOne
     @JoinColumn(name = "building_id", nullable = false)
     private Building building;
+
+    @OneToMany(mappedBy = "significant")
+    private List<SignificantImage> images = new ArrayList<>();
 }
---
+++ src/main/java/ganaljigi/entity/domain/SignificantImage.java
@@ -0,0 +1,21 @@
+package ganaljigi.entity.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SignificantImage {
+
+    @Id @GeneratedValue
+    @Column(name = "significant_image_id")
+    private Long id;
+
+    private String url; // 사진 url
+
+    @ManyToOne
+    @JoinColumn(name = "significant_id", nullable = false)
+    private Significant significant;
+}
